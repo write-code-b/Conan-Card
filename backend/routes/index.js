3a72e099769ce792e86b64563601683e8d6be7b8
write-code-b <minjichoi.official@gmail.com>
2024-07-10 14:34:38 +0900

    add get card_list routes

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,14 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+
+const card_controller = require("../controllers/cardController");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+// GET request for list of all Card.
+router.get("/cards", card_controller.card_list);
 
 module.exports = router;
