c43f68f1653afc0d26deeb07233ae3ea4d057f9e
write-code-b <minjichoi.official@gmail.com>
2024-07-15 14:43:00 +0900

    build header, section, footer layout

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <nav></nav>
+      <section id="cardSearch"></section>
+      <section id="cardList"></section>
+      <footer></footer>
+    </div>
+  );
+}
+
+export default App;
