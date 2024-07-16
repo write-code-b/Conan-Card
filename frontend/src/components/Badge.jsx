3d3c65fe79f19f980c0336fb5540bd2028ce68d7
write-code-b <minjichoi.official@gmail.com>
2024-07-16 16:29:13 +0900

    update show result on CardSearch Component

--- frontend/src/components/Badge.jsx
+++ frontend/src/components/Badge.jsx
@@ -1,15 +1,26 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function Badge(props) {
   const [click, setClick] = useState(false);
-  function clickSearchTag() {
-    return click ? setClick(false) : setClick(true);
+
+  function clickSearchTag(e) {
+    if (props.event) {
+      const value = e.target.innerHTML;
+      if (props.tags.includes(value)) {
+        props.tags.splice(props.tags.indexOf(value), 1);
+      } else {
+        props.setTags([...props.tags, value]);
+      }
+      props.setShowResult(false);
+      return click ? setClick(false) : setClick(true);
+    }
   }
+
   return (
     <>
       <div
         className={`badge ${click ? "clicked" : ""}`}
-        onClick={clickSearchTag}
+        onClick={(e) => clickSearchTag(e)}
       >
         {props.value}
       </div>
--- frontend/src/components/CardSerach.jsx
+++ frontend/src/components/CardSerach.jsx
@@ -1,18 +1,51 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Card from "./Card";
 import Badge from "./Badge";
 
 function CardSearch(props) {
+  const [tags, setTags] = useState([]);
+  const [showResult, setShowResult] = useState(false); //true -> show, false -> hide
   const color = ["적", "황", "녹", "청", "백", "흑"];
   const category = ["사건", "이벤트", "캐릭터", "파트너"];
   const rarity = ["C", "CP", "R", "RP", "SRP", "D", "PR"];
 
-  const colorList = color.map((badge) => <Badge value={badge} />);
-  const categoryList = category.map((badge) => <Badge value={badge} />);
-  const rarityList = rarity.map((badge) => <Badge value={badge} />);
+  const colorList = color.map((badge) => (
+    <Badge
+      value={badge}
+      tags={tags}
+      setTags={setTags}
+      event={true}
+      setShowResult={setShowResult}
+    />
+  ));
+  const categoryList = category.map((badge) => (
+    <Badge
+      value={badge}
+      tags={tags}
+      setTags={setTags}
+      event={true}
+      setShowResult={setShowResult}
+    />
+  ));
+  const rarityList = rarity.map((badge) => (
+    <Badge
+      value={badge}
+      tags={tags}
+      setTags={setTags}
+      event={true}
+      setShowResult={setShowResult}
+    />
+  ));
+  const tagSearchList = tags.map((badge) => (
+    <Badge value={badge} event={false} />
+  ));
 
   function onChange() {
     props.flipAllCard();
+  }
+
+  function clickSearchBtn() {
+    setShowResult(true);
   }
 
   return (
@@ -51,11 +84,14 @@ function CardSearch(props) {
             <div className="rarity">{rarityList}</div>
           </div>
         </div>
-        <button id="searchBtn">
+        <button id="searchBtn" onClick={clickSearchBtn}>
           검색
         </button>
         <div className="result">
-          <div>{props.count}개</div>
+          <div>
+            {props.count}개
+            <div className="tags">{showResult && tagSearchList}</div>
+          </div>
           <div className="flip">
             <input
               type="checkbox"
