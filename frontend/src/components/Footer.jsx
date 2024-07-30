7485e78514775eb37bf3b12d21da1cd316421ecf
write-code-b <minjichoi.official@gmail.com>
2024-07-30 14:03:06 +0900

    update Footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,13 +1,22 @@
 function Footer() {
   return (
     <div id="footer">
-      <p>
-        Detective Conan © by Gosho Aoyama/Shogakukan. Detective Conan TCG © by
-        TOMY
-      </p>
-      <p>명탐정 코난 카드게임을 위한 팬페이지입니다.</p>
+      <div>
+        <p>
+          Detective Conan © by Gosho Aoyama/Shogakukan. Detective Conan TCG © by
+          TOMY
+        </p>
+        <p>명탐정 코난 카드게임을 위한 팬페이지입니다.</p>
+      </div>
       <div>
         <p>카드 일한 번역자_장윤식</p>
+      </div>
+      <div>
+        <a target="_blank" href="https://icons8.com">
+          icon by Icons8
+        </a>
+      </div>
+      <div>
         <a
           href="https://github.com/write-code-b/Conan-Card"
           className="github"
