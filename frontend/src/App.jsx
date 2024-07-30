8e39a7f7045d255d59a89e2bcd781b3ce68c18f6
write-code-b <minjichoi.official@gmail.com>
2024-07-30 14:33:48 +0900

    add About page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { Route, Routes, Navigate } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import About from "./components/About";
 import CardList from "./components/CardList";
 import Footer from "./components/Footer";
 import "./App.scss";
@@ -10,6 +11,7 @@ function App() {
     <div className="App">
       <Navbar />
       <Routes>
+        <Route path="/about" element={<About />} />
         <Route path="/cards" element={<CardList />} />
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
---
+++ frontend/src/components/About.jsx
@@ -0,0 +1,12 @@
+function About() {
+  return (
+    <>
+      <div id="about">
+        <div>코난 카드를 한국어로 변역한 페이지입니다.</div>
+      </div>
+      <div></div>
+    </>
+  );
+}
+
+export default About;
