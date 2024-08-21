b466b706a3251ce6b6202575529e5ee910b214de
write-code-b <minjichoi.official@gmail.com>
2024-08-21 11:54:56 +0900

    update CardData

--- frontend/src/data/CardData.js
+++ frontend/src/data/CardData.js
@@ -8,24 +8,7 @@ const PRODUCT_LIST = [
   { id: 6, name: "CT-D03 Case-StartDeck 03 괴도 키드" },
   { id: 7, name: "CT-D04 Case-StartDeck 04 아카이 슈이치" },
   { id: 8, name: "CT-D05 Case-StartDeck 05 아무로 토오루" },
-  { id: 9, name: "프로모 카드(극장 예매 특전)" },
-  { id: 10, name: "프로모 카드(세븐 일레븐 캠페인)" },
-  { id: 11, name: "프로모 카드(소년 선데이-22,23 합병호 부록)" },
-  { id: 12, name: "프로모 카드(소년 선데이-37,38 합병호 부록)" },
-  { id: 13, name: "프로모 카드(소년 선데이-S 2024년 8월호 부록)" },
-  { id: 14, name: "프로모 카드(소년 선데이-S 2024년 7월호 부록)" },
-  { id: 15, name: "프로모 카드(소년 선데이-S 2024년 9월호 부록)" },
-  { id: 16, name: "프로모 카드(챌린지전 외)" },
-  { id: 17, name: "프로모 카드(월간 코로코로코믹스 2024년 6월호 부록)" },
-  { id: 18, name: "프로모 카드(프로모션 팩 Vol.1)" },
-  { id: 19, name: "프로모 카드(프로모션 팩 Vol.2)" },
-  { id: 20, name: "프로모 카드(CT-D01～CT-D05 Case-StartDeck 봉입 특전)" },
-  {
-    id: 21,
-    name: "프로모 카드(CT-P01 Case-Booster 01 탐정들의 조커 BOX 봉입 특전)",
-  },
-  { id: 22, name: "프로모 카드(주간 소년선데이-2024년 9호)" },
-  { id: 23, name: "프로모 카드" },
+  { id: 9, name: "프로모 카드" },
 ];
 
 const COLOR_LIST = [
@@ -46,12 +29,10 @@ const CATEGORY_LIST = [
 
 const RARITY_LIST = [
   { id: 10, name: "C" },
-  { id: 11, name: "CP" },
-  { id: 12, name: "R" },
-  { id: 13, name: "RP" },
-  { id: 14, name: "SRP" },
-  { id: 15, name: "D" },
-  { id: 16, name: "PR" },
+  { id: 11, name: "R" },
+  { id: 12, name: "SR" },
+  { id: 13, name: "D" },
+  { id: 14, name: "PR" },
 ];
 
 export { PRODUCT_LIST, COLOR_LIST, CATEGORY_LIST, RARITY_LIST };
