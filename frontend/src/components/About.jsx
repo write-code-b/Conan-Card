8945ff827e2d1cf589dde1297c0e1a3f9f29a160
write-code-b <minjichoi.official@gmail.com>
2024-08-21 15:02:38 +0900

    update about page

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -2,7 +2,17 @@ function About() {
   return (
     <>
       <div id="about">
-        <div>코난 카드를 한국어로 변역한 페이지입니다.</div>
+        <div>
+          코난 카드를 한국어로 변역한 페이지입니다. 번역 내용에 대해 피드백이
+          있다면&nbsp;
+          <a
+            target="_blank"
+            href="https://minjichoiofficial.notion.site/Conan-TCG-DECK-3417b0a9e96542858557171e28c2c277?pvs=4"
+          >
+            수정 요청 페이지(link)
+          </a>
+          에 댓글✏️을 달아주세요.
+        </div>
       </div>
       <div></div>
     </>
