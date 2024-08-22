7112287bde7a14ad52271003c4cdf9ca5fb6159a
write-code-b <minjichoi.official@gmail.com>
2024-08-22 15:34:04 +0900

    add sort query

--- backend/controllers/cardController.js
+++ backend/controllers/cardController.js
@@ -6,12 +6,22 @@ const asyncHandler = require("express-async-handler");
 // Display list of all cards.
 exports.card_list = asyncHandler(async (req, res, next) => {
   const query = req.query;
+  let sortFlag = false;
+  let sortKeyword = "_id";
+  let sortMethod = 1;
+
   if (Object.entries(query).length === 0) {
     const allCards = await Card.find({});
     res.json(allCards);
   } else {
     const regex = (pattern) => new RegExp(`.*${pattern}.*`);
     const filter = [query].map((k) => {
+      if (k.sort) {
+        sortFlag = true;
+        sortKeyword = k.sort && k.sort.split("_")[0];
+        sortMethod = k.sort && (k.sort.split("_")[1] === "high" ? "-1" : "1");
+      }
+
       return {
         ...(k.name && { name: { $regex: regex(k.name) } }),
         ...(k.code && { code: { $in: k.code } }),
@@ -21,7 +31,15 @@ exports.card_list = asyncHandler(async (req, res, next) => {
         ...(k.rarity && { rarity: { $in: k.rarity } }),
       };
     });
-    const filterCards = await Card.find(filter[0]);
-    res.json(filterCards);
+
+    if (sortFlag) {
+      const filterCards = await Card.find(filter[0]).sort({
+        [sortKeyword]: parseInt(sortMethod),
+      });
+      res.json(filterCards);
+    } else {
+      const filterCards = await Card.find(filter[0]);
+      res.json(filterCards);
+    }
   }
 });
