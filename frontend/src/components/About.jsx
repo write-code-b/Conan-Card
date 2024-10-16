c19464aba5b7b1cff4dd5d7e43dae75ea72fbe9b
write-code-b <minjichoi.official@gmail.com>
2024-10-16 13:42:15 +0900

    update about page

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -3,8 +3,8 @@ function About() {
     <>
       <div id="about">
         <div>
-          코난 카드를 한국어로 변역한 페이지입니다. 번역 내용에 대해 피드백이
-          있다면&nbsp;
+          코난 카드를 한국어로 변역한 페이지입니다. 번역 내용과 관련한 피드백은
+          환영입니다.&nbsp;
           <a
             target="_blank"
             href="https://crystal-quince-dc4.notion.site/Conan-TCG-DECK-fb1ca0b8f57f43a5ad679c861b05decd?pvs=4"
@@ -14,7 +14,6 @@ function About() {
           에 댓글✏️을 달아주세요.
         </div>
       </div>
-      <div></div>
     </>
   );
 }
